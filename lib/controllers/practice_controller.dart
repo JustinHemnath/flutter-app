59f48530fb342ba115cadc44356e1fafe4f8785a
Hemnath Balasubramanian <hemnath@theproindia.com>
2023-04-10 18:40:59 +0530

    10/4

--- lib/controllers/practice_controller.dart
+++ lib/controllers/practice_controller.dart
@@ -2,8 +2,17 @@ import "package:get/get.dart";
 
 class PracticeController extends GetxController {
   RxInt _count = 0.obs;
+  RxInt get count => _count;
 
   void increase() {
     _count++;
+  }
+
+  void reset() {
+    _count.value = 0;
+  }
+
+  void decrease() {
+    _count--;
   }
 }
--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -11,33 +11,75 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     PracticeController practicecontroller = Get.put(PracticeController());
+
     return Scaffold(
-      appBar: AppBar(title: Text("HOME")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
+        appBar: AppBar(
+          title: const Text(
+            "HOME",
+            style: TextStyle(color: Colors.yellow),
+          ),
+          backgroundColor: Colors.red[900],
+        ),
+        body: Container(
+          width: double.infinity,
+          color: Colors.grey[100],
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Column(
+              Obx(
+                () => Text(
+                  practicecontroller.count.value.toString(),
+                  style: const TextStyle(
+                    fontSize: 60.0,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  ElevatedButton(
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                      color: Colors.red[900],
+                    ),
+                    child: IconButton(
+                      color: Colors.white,
                       onPressed: () {
                         practicecontroller.increase();
                       },
-                      child: Text("Increase")),
+                      icon: const Icon(Icons.add_rounded),
+                    ),
+                  ),
                   ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red[900],
+                    ),
+                    onPressed: () {
+                      practicecontroller.reset();
+                    },
+                    child: const Text("RESET"),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                      color: Colors.red[900],
+                    ),
+                    child: IconButton(
+                      color: Colors.white,
                       onPressed: () {
-                        Get.to(Practicepage());
+                        practicecontroller.decrease();
                       },
-                      child: Text("Practice page"))
+                      icon: const Icon(Icons.remove),
+                    ),
+                  )
                 ],
-              ),
+              )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        ));
   }
 }
